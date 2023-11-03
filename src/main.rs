5107a1889be6138c7372164009a28999d3956da8
DaniSancas <lord.sancas@gmail.com>
2023-11-03 23:04:34 +0100

    chore: format code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::env;
-use std::process::exit;
 use colored::*;
 use indicatif::ProgressBar;
 use polars::frame::DataFrame;
-use polars::prelude::{col, IndexOfSchema, IntoVec, LazyCsvReader, LazyFileListReader, LazyFrame, SortOptions};
+use polars::prelude::{
+    col, IndexOfSchema, IntoVec, LazyCsvReader, LazyFileListReader, LazyFrame, SortOptions,
+};
+use std::env;
+use std::process::exit;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const EXECUTABLE_NAME: &str = env!("CARGO_PKG_NAME");
@@ -27,15 +29,24 @@ fn main() {
     let second_file_cols = get_column_names(&second_file_lf);
 
     if first_file_cols.len() != second_file_cols.len() {
-        println!("{} : {}","FILES ARE DIFFERENT".red(), "Different number of columns".on_bright_red());
+        println!(
+            "{} : {}",
+            "FILES ARE DIFFERENT".red(),
+            "Different number of columns".on_bright_red()
+        );
         exit(2);
     }
 
     for i in 0..first_file_cols.len() {
         if first_file_cols[i] != second_file_cols[i] {
-            println!("{}: {} #{} => {} != {}",
-                "FILES ARE DIFFERENT".red(), "Different names for column".red(),
-                     i+1, first_file_cols[i].bold().yellow(), second_file_cols[i].bold().blue());
+            println!(
+                "{}: {} #{} => {} != {}",
+                "FILES ARE DIFFERENT".red(),
+                "Different names for column".red(),
+                i + 1,
+                first_file_cols[i].bold().yellow(),
+                second_file_cols[i].bold().blue()
+            );
             exit(2);
         }
     }
@@ -43,22 +54,28 @@ fn main() {
     let sorting_column = &first_file_cols[0];
     let columns_to_iterate = (first_file_cols.len() - 1) as u64;
 
-    println!("Comparing content of each column in both files when sorted by column: {} ...", sorting_column);
+    println!(
+        "Comparing content of each column in both files when sorted by column: {} ...",
+        sorting_column
+    );
     let progress_bar = ProgressBar::new(columns_to_iterate);
     for i in 1..first_file_cols.len() {
         let column_name = &first_file_cols[i];
 
-        let first_data_frame = get_sorted_data_frame_for_column(&first_file_lf,
-                                                          sorting_column, column_name);
+        let first_data_frame =
+            get_sorted_data_frame_for_column(&first_file_lf, sorting_column, column_name);
 
-        let second_data_frame = get_sorted_data_frame_for_column(&second_file_lf,
-                                                           sorting_column, column_name);
+        let second_data_frame =
+            get_sorted_data_frame_for_column(&second_file_lf, sorting_column, column_name);
 
         if !first_data_frame.frame_equal_missing(&second_data_frame) {
-            println!("{}: {} {} {}", "FILES ARE DIFFERENT".red(),
-                     "Values for column".red(),
-                     column_name.on_bright_red(),
-                     "are different".red());
+            println!(
+                "{}: {} {} {}",
+                "FILES ARE DIFFERENT".red(),
+                "Values for column".red(),
+                column_name.on_bright_red(),
+                "are different".red()
+            );
 
             exit(3);
         }
@@ -66,7 +83,11 @@ fn main() {
     }
     progress_bar.finish();
 
-    println!("{} {}", "FILES ARE IDENTICAL WHEN SORTED BY COLUMN:".green(), sorting_column.green());
+    println!(
+        "{} {}",
+        "FILES ARE IDENTICAL WHEN SORTED BY COLUMN:".green(),
+        sorting_column.green()
+    );
 }
 
 fn get_lazy_frame(file_path: &str) -> LazyFrame {
@@ -78,7 +99,8 @@ fn get_lazy_frame(file_path: &str) -> LazyFrame {
 }
 
 fn get_column_names(lazy_frame: &LazyFrame) -> Vec<String> {
-    let schema = lazy_frame.clone()
+    let schema = lazy_frame
+        .clone()
         .limit(1)
         .collect()
         .expect("Couldn't parse first CSV file")
@@ -87,8 +109,13 @@ fn get_column_names(lazy_frame: &LazyFrame) -> Vec<String> {
     schema.get_names().into_vec()
 }
 
-fn get_sorted_data_frame_for_column(lazy_frame: &LazyFrame, sorting_column: &String, column: &String) -> DataFrame {
-    lazy_frame.clone()
+fn get_sorted_data_frame_for_column(
+    lazy_frame: &LazyFrame,
+    sorting_column: &String,
+    column: &String,
+) -> DataFrame {
+    lazy_frame
+        .clone()
         .select([col(sorting_column), col(column)])
         .sort(sorting_column, SortOptions::default())
         .collect()
